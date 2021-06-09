ec2886f0bd3a5bbacbaf3cd8c4c6ba85343f85e9
John Atkins <JDAtkins@tql.com>
2021-06-09 14:08:43 -0400

    Add project files.

---
+++ EfEdDb/Program.cs
@@ -0,0 +1,37 @@
+﻿using EfEdDb.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EfEdDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new EdDbContext();
+
+            var studentsWithMajors = from s in context.Students
+                                     join m in context.Majors
+                                     on s.MajorId equals m.Id
+                                     where s.StateCode == "KY"
+                                     orderby s.Lastname
+                                     select new { s.Firstname, s.Lastname, Major = m.Description
+                                     };
+            foreach(var s in studentsWithMajors)
+            {
+                Console.WriteLine($"{s.Firstname} {s.Lastname} || {s.Major}");
+            }
+            
+            //var students = context.Students
+            //    .Where(s => s.StateCode == "KY")
+            //    .OrderBy(s => s.Lastname)
+            //    .ToList();
+            
+            //foreach(var s in students)
+            //{
+            //    Console.WriteLine($"{s.Firstname} {s.Lastname}");
+            //}
+        }
+    }
+}
